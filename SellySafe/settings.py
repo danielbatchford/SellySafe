267268d3037f4fbf26edf03cfcef54f78111e102
Daniel Batchford <danielbatchford@gmail.com>
2020-07-03 23:18:36 +0100

    Added Markers from DB and Popups

--- SellySafe/settings.py
+++ SellySafe/settings.py
@@ -15,21 +15,21 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ejpmij&_nr2(*zv^o!!r=uk_&5i5_kv=#w90ue$b7^c&&kg%s-'
 
-MAP_BOX_KEY = 'pk.eyJ1IjoiZGFuaWVsYmF0Y2hmb3JkIiwiYSI6ImNrYzZoNmNkZjByaWwydnJpdjB4OHc2bW0ifQ.NL5rP5elBSBPMA0bE9x4fQ'
+MAPBOX_KEY = 'pk.eyJ1IjoiZGFuaWVsYmF0Y2hmb3JkIiwiYSI6ImNrYzZoNmNkZjByaWwydnJpdjB4OHc2bW0ifQ.NL5rP5elBSBPMA0bE9x4fQ'
 
+CITY_CENTER = [-1.930965, 52.442965]
+BOUNDS = [[-1.955059, 52.428762], [-1.912666, 52.454658]]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SellySafe.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -84,7 +83,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -117,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- sellysafeweb/views.py
+++ sellysafeweb/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
 from SellySafe import settings
+from sellysafeweb.forms import ReportForm
+from sellysafeweb.models import Report
 
 
 def map(request):
-    return render(request, 'sellysafeweb/map.html', {'map_box_key': settings.MAP_BOX_KEY})
+    form = ReportForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return render(request, 'sellysafeweb/confirmation.html')
+
+    reports = Report.objects.all()
+    return render(request, 'sellysafeweb/map.html', {
+        'reports': reports,
+        'form': ReportForm(),
+        'map_box_key': settings.MAPBOX_KEY,
+        'city_center': settings.CITY_CENTER,
+        'bounds': settings.BOUNDS})
